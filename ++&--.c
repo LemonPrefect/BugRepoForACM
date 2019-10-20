923d54f80ceec9495995ab5f1e905b819cab3b60
Jingzhuo Kwok <jingzhuokwok@qq.com>
2019-10-20 19:09:04 +0800

    Change Directory

--- ++&--.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a = 15;
-    printf("a--=%d,a=%d\n",a--,a);
-    a = 15;
-    printf("--a=%d,a=%d\n",--a,a);
-    return 0;
-}
--- 6.16.1.c
+++
@@ -1,8 +0,0 @@
-//C PRIMER PLUS 6
-//6.16.1
-#include <stdio.h>
-int main(){
-	char alphabetArray[27] = "abcdefghijklmnopqrstuvwxyz";
-	printf("%s",alphabetArray);
-	return 0;
-} 
